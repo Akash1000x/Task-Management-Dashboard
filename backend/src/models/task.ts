81f0ca35bf6394755fa57aa334cc5633f73f209b
Akash kumar kumawat <kumawatap63@gmail.com>
2024-09-20 22:32:02 +0530

    backend: Added task model

---
+++ backend/src/models/task.ts
@@ -0,0 +1,39 @@
+import mongoose, { Document, Schema } from "mongoose";
+import { IUser } from "./user";
+
+export enum Status {
+  ToDO = "To Do",
+  InProgress = "In Progress",
+  Completed = "Completed",
+}
+
+export enum Priority {
+  Low = "Low",
+  Medium = "Medium",
+  High = "High",
+}
+
+interface ITask extends Document {
+  title: string;
+  description: string;
+  status: Status;
+  priority: Priority;
+  dueDate: Date;
+  user: IUser["_id"];
+}
+
+const TaskSchema: Schema<ITask> = new Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    status: { type: String, enum: Object.values(Status), default: Status.ToDO, required: true },
+    priority: { type: String, enum: Object.values(Priority), default: Priority.Medium, required: true },
+    dueDate: { type: Date, required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  },
+  { timestamps: true }
+);
+
+const Task = mongoose.model<ITask>("Task", TaskSchema);
+
+export default Task;
