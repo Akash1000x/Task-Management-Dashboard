26bb6d65ca15c5c302abcb7c4e742c794c857581
Akash kumar kumawat <kumawatap63@gmail.com>
2024-09-20 22:14:49 +0530

    backend: Added main index file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,24 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import "dotenv/config";
+import routes from "./routes/index";
+import { connectDB } from "./db";
+
 const app = express();
-const port = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
+app.use(cookieParser());
 
+connectDB();
+
+//health check
 app.get("/", (req, res) => {
-  return res.json("Hello World!");
+  return res.status(200).json({ message: "Server is running" });
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+app.use("/api/v1", routes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
