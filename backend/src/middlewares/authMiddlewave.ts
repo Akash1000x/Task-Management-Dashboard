b3c0988e5cb32931ec1ca77f4401cb70c203c6ba
Akash kumar kumawat <kumawatap63@gmail.com>
2024-09-20 22:33:56 +0530

    backend: Added auth middleware

---
+++ backend/src/middlewares/authMiddlewave.ts
@@ -0,0 +1,28 @@
+import User from "../models/user.js";
+import jwt from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
+
+export const verifyJWT = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const token = req.cookies?.token;
+
+    if (!token) {
+      return res.status(401).json({ status: false, message: "Not authorized. Try login again." });
+    }
+
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET as string) as { userId: string };
+
+    const user = await User.findById(decodedToken?.userId);
+
+    if (!user) {
+      return res.status(401).json({ status: false, message: "Not authorized. Try login again." });
+    }
+
+    req.user = user;
+    next();
+  } catch (error: any) {
+    console.log(error);
+
+    return res.status(401).json({ status: false, message: "Not authorized. Try login again." });
+  }
+};
