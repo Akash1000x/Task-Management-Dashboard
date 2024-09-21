1da98e7a1e1f55c69c671f88e1c032e41f6b007d
Akash kumar kumawat <kumawatap63@gmail.com>
2024-09-22 03:44:34 +0530

    updated api url

--- frontend/lib/config.ts
+++ frontend/lib/config.ts
@@ -1,7 +1,7 @@
 import { TaskPriority, TaskStatus } from "./types";
 
 export const ApiUrl: string =
-  process.env.NODE_ENV === "production" ? "http://3.110.104.66/api/v1" : "http://localhost:8000/api/v1";
+  process.env.NODE_ENV === "production" ? "https://task.akashdev.me/api/v1" : "http://localhost:8000/api/v1";
 
 export const priorityOptions: TaskPriority[] = ["Low", "Medium", "High"];
 export const statusOptions: TaskStatus[] = ["To Do", "In Progress", "Completed"];
