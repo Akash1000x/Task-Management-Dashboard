ac3f149abec60b63b1934b12699e2bd4a888d30a
Akash kumar kumawat <kumawatap63@gmail.com>
2024-09-22 00:44:40 +0530

    Added custom tab bar component

---
+++ frontend/components/custom-tab-bar.tsx
@@ -0,0 +1,36 @@
+"use client";
+import * as React from "react";
+import CreateTask from "@/components/create-task-dialog";
+import { Button } from "@/components/ui/button";
+import { Combobox } from "./ui/combobox";
+import { useTaskFilter } from "@/hooks/useTaskFilter";
+
+export default function CustomTabBar() {
+  const [filterOption, setFilterOption] = React.useState<string>("");
+  const [isDialogOpe, setIsDialogOpe] = React.useState<boolean>(false);
+  const filterOptions = ["status", "priority", "date"];
+
+  const { sortByDate, sortByStatus, sortByPriority } = useTaskFilter();
+
+  React.useEffect(() => {
+    if (filterOption === "date") {
+      sortByDate();
+    } else if (filterOption === "priority") {
+      sortByPriority();
+    } else if (filterOption === "status") {
+      sortByStatus();
+    }
+  }, [filterOption]);
+
+  return (
+    <>
+      <div className="flex justify-between sm:pb-4 pb-2">
+        <div className="flex font-bold gap-4 items-center">
+          <Combobox data={filterOptions} value={filterOption} setValue={setFilterOption} />
+        </div>
+        <Button onClick={() => setIsDialogOpe(true)}>Create Task</Button>
+      </div>
+      {isDialogOpe && <CreateTask isDialogOpen={isDialogOpe} setIsDialogOpen={setIsDialogOpe} />}
+    </>
+  );
+}
