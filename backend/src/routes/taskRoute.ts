bdad34d43fa5826369889526c11b0e38374cdbbe
Akash kumar kumawat <kumawatap63@gmail.com>
2024-09-22 00:35:24 +0530

    backend: Refactor taskRoute to use kebab-case for get-tasks endpoint

--- backend/src/routes/taskRoute.ts
+++ backend/src/routes/taskRoute.ts
@@ -5,7 +5,7 @@ import { verifyJWT } from "../middlewares/authMiddlewave";
 const router = express.Router();
 
 router.post("/create", verifyJWT, createTask);
-router.get("/getTasks", verifyJWT, getTasks);
+router.get("/get-tasks", verifyJWT, getTasks);
 router.put("/update/:id", verifyJWT, updateTask);
 router.put("/delete/:id", verifyJWT, deleteTask);
 
