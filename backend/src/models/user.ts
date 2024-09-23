5f7180445e4175c1e1e063b037226e1d9302da9a
Akash kumar kumawat <kumawatap63@gmail.com>
2024-09-23 18:07:09 +0530

    backend: Add email  index in UserSchema

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -15,7 +15,7 @@ export interface IUser extends Document {
 const UserSchema: Schema<IUser> = new Schema(
   {
     name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true, index: true, trime: true },
     password: { type: String, required: true },
     tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: "Task" }],
   },
